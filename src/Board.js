2d91d3c22eeea56f11b3396f78dbd89c6b94d83f
TejasPatel2000 <patel.tejas2000@gmail.com>
2021-02-20 22:53:48 +0000

    Updated Board.js for socket communication

--- src/Board.js
+++ src/Board.js
@@ -14,6 +14,7 @@ export function Board() {
       setBoard(prevBoard => { 
           let newBoard=[...prevBoard];
           var player=isX ? "X" : "O";
+          console.log("Player", player);
           newBoard[index]=player;
           return newBoard;
       });
