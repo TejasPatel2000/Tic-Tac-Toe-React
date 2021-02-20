bcd94015ebf150a62e69d4adee09c7dbd74204a5
TejasPatel2000 <patel.tejas2000@gmail.com>
2021-02-20 22:11:10 +0000

    Updated gitignore and added board to app.js

--- src/App.js
+++ src/App.js
@@ -1,50 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 import { ListItem } from './ListItem.js';
-import { useState, useRef, useEffect } from 'react';
-import io from 'socket.io-client';
+import { useState, useRef } from 'react';
 
-const socket = io(); // Connects to socket connection
+import { Board } from './Board.js';
 
 function App() {
-  const [messages, setMessages] = useState([]); // State variable, list of messages
-  const inputRef = useRef(null); // Reference to <input> element
+  const [myList, changeList]  = useState([]);
+  const inputRef = useRef(null);
 
+  
   function onClickButton() {
-    if (inputRef != null) {
-      const message = inputRef.current.value;
-      // If your own client sends a message, we add it to the list of messages to 
-      // render it on the UI.
-      setMessages(prevMessages => [...prevMessages, message]);
-      socket.emit('chat', { message: message });
-    }
+    const userText = inputRef.current.value;
+    changeList(prevList => [...myList, userText]);
   }
-
-  // The function inside useEffect is only run whenever any variable in the array
-  // (passed as the second arg to useEffect) changes. Since this array is empty
-  // here, then the function will only run once at the very beginning of mounting.
-  useEffect(() => {
-    // Listening for a chat event emitted by the server. If received, we
-    // run the code in the function that is passed in as the second arg
-    socket.on('chat', (data) => {
-      console.log('Chat event received!');
-      console.log(data);
-      // If the server sends a message (on behalf of another client), then we
-      // add it to the list of messages to render it on the UI.
-      setMessages(prevMessages => [...prevMessages, data.message]);
-    });
-  }, []);
+  
+  const [board, setBoard] = useState([]);
+  
 
   return (
     <div>
-      <h1>Chat Messages</h1>
-      Enter message here: <input ref={inputRef} type="text" />
-      <button onClick={onClickButton}>Send</button>
-      <ul>
-        {messages.map((item, index) => <ListItem key={index} name={item} />)}
-      </ul>
+    <h1> Tic Tac Toe </h1>
+    
+    <Board />
+    
     </div>
   );
+    // <input ref = {inputRef} type = "text" />
+    // <button onClick={onClickButton} >Add to list!</button>
+    // <ul>
+    //   {myList.map(item => <ListItem name={item} />)}
+    // </ul>
 }
 
 export default App;
