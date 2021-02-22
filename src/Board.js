89ebf9ce54db25827c20f121368d32c55eea079a
TejasPatel2000 <patel.tejas2000@gmail.com>
2021-02-22 21:28:53 +0000

    Finished HW 10(Updated socket communication so board updates on other browsers

--- src/Board.js
+++ src/Board.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import './Board.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Square } from './Square.js';
+import io from 'socket.io-client';
+
+const socket = io(); // Connects to socket connection
 
 export function Board() {
     const [board, setBoard] = useState([]);
-    const [isX, setXNext ] = useState(true); // state2 variable is a number
-
+    const [isX, setXNext ] = useState(true); 
+    
     function fillSquare(index){
-      const newBoard = [...board];
-      newBoard[index] = isX ? "X" : "O";
-      setXNext(!isX);
-      setBoard(newBoard);
-     // setBoard(prevBoard =>[index, "X"]);
-    }
+      setBoard(prevBoard => { 
+          let newBoard=[...prevBoard];
+          var player=isX ? "X" : "O";
+          newBoard[index]=player;
+          return newBoard;
+      });
+      socket.emit('square', { index: index });
+      // var player = isX ? "X" : "O";
+      // return setBoard(prevBoard => [...prevBoard], board[index]=player );
+ }
+    
+    
+     useEffect(() => {
+    // Listening for a chat event emitted by the server. If received, we
+    // run the code in the function that is passed in as the second arg
+        socket.on('square', (data) => {
+          console.log('click event received!');
+          console.log(data);
+        
+          // If the server sends a message (on behalf of another client), then we
+          // add it to the list of messages to render it on the UI.
+          
+          setBoard(prevBoard => { 
+            let newBoard=[...prevBoard];
+            newBoard[data.index]=isX ? "X" : "O";
+            console.log("SErver", newBoard);
+            // setXNext(!isX);
+            return newBoard;
+          });
+            
+            
+          //var player = isX ? "X" : "O";
+          //return setBoard(prevBoard => [...prevBoard], board[data.index]=player);
+          
+        });
+    }, []);
+
+  
     
     return <div class="board">
         <Square fillSquare={fillSquare} board={board} index={0} />
